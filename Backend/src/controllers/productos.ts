88a3c4ebb1c2d9779b613342abb32c4bc02f5e43
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-06 17:09:23 -0300

    implementando el manejador de errores

--- Backend/src/controllers/productos.ts
+++ Backend/src/controllers/productos.ts
@@ -1,20 +1,20 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { ProductosModel } from "../models/productos.js";
 import { ValidateId } from "../schemas/productos.js";
 
 export class ProductosController{
-    static async getAll(req: Request, res: Response){
+    static async getAll(req: Request, res: Response, next: NextFunction){
         const categoria = typeof req.query.categoria === 'string'
         ? req.query.categoria
         : undefined
         try{
             const productos = await ProductosModel.getAll({ categoria })
-            res.status(200).json(productos)
-        }catch(error){
-            if(error instanceof Error) res.status(500).json({message: "Error de servidor"})
+            return res.status(200).json(productos)
+        }catch(err){
+            return next(err)
         }
     }
-    static async getById(req: Request, res: Response){
+    static async getById(req: Request, res: Response, next: NextFunction){
         const { id } = req.params as {id: string}
         const result = ValidateId(id)
         if(!result.success) return res.status(400).json({message: "El id es invalido"})
@@ -22,8 +22,8 @@ export class ProductosController{
             const producto = await ProductosModel.getById({ id: result.data })
             if(producto === null) return res.status(404).json({message: "El producto no existe"})
             return res.status(200).json(producto)
-        }catch(error){
-            return res.status(500).json({message: "Error de servidor"})
+        }catch(err){
+            return next(err)
         }
     }
     // static async create(req: Request, res: Response){
--- Backend/src/middlewares/errorHandler.ts
+++ Backend/src/middlewares/errorHandler.ts
@@ -1,11 +1,3 @@
-// export function errorHandler(err, req, res, next){
-//     console.error('[ERROR]', err.message, err.stack)
-//     res.status(500).json({
-//         message: 'Internal Server Error',
-//         error: process.env.NODE_ENV === 'development' ? err.message : undefined
-//     })
-// }
-
 import type { NextFunction, Request, Response } from "express"
 
 export function errorHandler(err: Error, _req: Request, res: Response, _next: NextFunction){
