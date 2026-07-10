0de1faa5bcfa28f0846b50f5e7580da414a2791c
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-10 12:35:25 -0300

    desarrollando metodo login en el modelo de auth

--- Backend/src/models/auth.ts
+++ Backend/src/models/auth.ts
@@ -1,5 +1,5 @@
 import { pool } from "../config/db.js";
-import type { RegisterInput } from "../schemas/auth.js";
+import type { LoginInput, RegisterInput } from "../schemas/auth.js";
 import bcrypt from "bcrypt"
 
 export class AuthModel{
@@ -27,7 +27,28 @@ export class AuthModel{
         return crearUsuario.rows[0]
     }
 
-    static async login(){}
+    static async login(input: LoginInput){
+        const { email, password } = input
+
+        const usuario = await pool.query(
+            `SELECT id, nombre, password FROM usuario 
+            WHERE email = $1
+            `, [email]
+        )
+
+        if(usuario.rows.length === 0) return null
+
+        const hashedPassword = usuario.rows[0].password
+
+        const validPassword = await bcrypt.compare(password, hashedPassword)
+
+        if(!validPassword) return null
+
+        const {password: _, ...usuarionSinPassword } = usuario.rows[0]
+
+        return usuarionSinPassword
+    }
+
     static async refresh(){}
     static async logout(){}
     static async forgotPassword(){}
--- Backend/src/schemas/auth.ts
+++ Backend/src/schemas/auth.ts
@@ -1,4 +1,4 @@
-import z from "zod"
+import z, { email } from "zod"
 
 const registerSchema = z.object({
     nombre: z.string().min(3),
@@ -6,8 +6,18 @@ const registerSchema = z.object({
     password: z.string().min(6).max(50) 
 }) 
 
+const loginSchema = z.object({
+    email: email(),
+    password: z.string().min(6).max(50) 
+})
+
 export type RegisterInput = z.infer<typeof registerSchema>
+export type LoginInput = z.infer<typeof loginSchema>
 
 export function ValidateRegister(input: unknown){
     return registerSchema.safeParse(input)
+}
+
+export function ValidateLogin(input: unknown){
+    return loginSchema.safeParse(input)
 }
