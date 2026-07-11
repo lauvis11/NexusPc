fc95558ff57267dafa4e1185394fca19436aaf67
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-11 12:46:54 -0300

    desarrollando middleware para verificar el rol

---
+++ Backend/src/middlewares/verifyAdmin.ts
@@ -0,0 +1,9 @@
+import type { NextFunction, Request, Response } from "express";
+
+export function VerifyRol(rol: string){
+    return function(req: Request, res: Response, next: NextFunction){
+        if(!req.usuario) return res.status(401).json({message: "Acceso no autorizado"})
+        if(req.usuario.rol !== rol) return res.status(403).json({ message: 'No tenés permisos para realizar esta acción' })
+        return next()
+    }
+}
