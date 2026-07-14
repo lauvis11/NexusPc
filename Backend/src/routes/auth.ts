96fdfb5f2c79f0ade062a4d672854363f3462c8a
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-14 14:08:49 -0300

    agregando midleware a la ruta y probando el endpoint

--- Backend/src/routes/auth.ts
+++ Backend/src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.js";
+import { VerifyToken } from "../middlewares/verifyToken.js";
 export const authRouter: Router = Router()
 
 authRouter.post("/login", AuthController.login)
 authRouter.post("/register", AuthController.register)
-authRouter.post("/logout", AuthController.logout)
+authRouter.post("/logout", VerifyToken, AuthController.logout)
 authRouter.post("/refresh", AuthController.refresh)
 authRouter.post("/forgot-password", AuthController.forgotPassword)
 
