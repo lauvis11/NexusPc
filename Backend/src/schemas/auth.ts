e5445c2b1b1c30de48f285fe3ebaed0f3cd44a40
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-15 11:30:48 -0300

    desarrollando metodo del controlador para actualizar la contraseña

--- Backend/src/schemas/auth.ts
+++ Backend/src/schemas/auth.ts
@@ -11,6 +11,11 @@ const loginSchema = z.object({
     password: z.string().min(6).max(50) 
 })
 
+const newPasswordSchema = z.object({
+    password: z.string().min(6).max(50),
+    newPassword: z.string().min(6).max(50)
+})
+
 export type RegisterInput = z.infer<typeof registerSchema>
 export type LoginInput = z.infer<typeof loginSchema>
 
@@ -20,4 +25,8 @@ export function ValidateRegister(input: unknown){
 
 export function ValidateLogin(input: unknown){
     return loginSchema.safeParse(input)
+}
+
+export function ValidateNewPassword(input: unknown){
+    return newPasswordSchema.safeParse(input)
 }
