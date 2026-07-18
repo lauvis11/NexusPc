b00e7d5e97cdc4b22e501e289009884d04e8f8f8
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-18 15:32:32 -0300

    desarrollando metodo get del controlador para ver las ordenes

--- Backend/src/controllers/ordenes.ts
+++ Backend/src/controllers/ordenes.ts
@@ -1,3 +1,19 @@
+import type { NextFunction, Request, Response } from "express";
+import { OrdenesModel } from "../models/ordenes.js";
+
 export class OrdenesController{
-    static async getAll(){}
+    static async getAll(req: Request, res: Response, next: NextFunction){
+        const usuario = req.usuario
+        if(!usuario) return res.status(401).json({message: 'Acceso no autorizado'})
+        try{
+            if(usuario.rol === 'ADMIN') {
+                const ordenes = await OrdenesModel.getAll()
+                return res.status(200).json(ordenes)
+            }   
+            const ordenesPorUsuario = await OrdenesModel.getByUsuario(usuario.id)
+            return res.status(200).json(ordenesPorUsuario)
+        }catch(err){
+            return next(err)
+        }
+    }
 }
