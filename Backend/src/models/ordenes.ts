625a3daf4dab4f431cfee2ac238b2e71b0dcddd8
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-18 15:22:45 -0300

    desarrollando metodo get del modelo para ver las ordenes de un usuario

--- Backend/src/models/ordenes.ts
+++ Backend/src/models/ordenes.ts
@@ -9,7 +9,7 @@ export class OrdenesModel{
                 orden.total, 
                 orden.created_at, 
                 usuario.nombre AS usuario_nombre, 
-                usuario.email AS usuario_gmail, 
+                usuario.email AS usuario_email, 
                 json_agg(
                     json_build_object(
                         'nombre', producto.nombre,
@@ -26,6 +26,37 @@ export class OrdenesModel{
                 usuario.nombre,
                 usuario.email
             ORDER BY orden.created_at DESC`
+        )
+
+        return ordenes.rows
+    }
+
+    static async getByUsuario(id: number){
+        const ordenes = await pool.query(
+            `SELECT 
+                orden.id, 
+                orden.estado, 
+                orden.total, 
+                orden.created_at, 
+                usuario.nombre AS usuario_nombre, 
+                usuario.email AS usuario_email, 
+                json_agg(
+                    json_build_object(
+                        'nombre', producto.nombre,
+                        'cantidad' detalle_orden.cantidad,
+                        'precio_unitario' detalle_orden.precio_unitario
+                    )
+                ) AS productos
+            FROM orden
+            JOIN usuario ON usuario.id = orden.usuario_id
+            JOIN detalle_orden ON detalle_orden.orden_id = orden.id
+            JOIN producto ON producto.id = detalle_orden.producto_id
+            WHERE usuario.id = $1
+            GROUP BY 
+                orden.id,
+                usuario.nombre,
+                usuario.email
+            ORDER BY orden.created_at DESC`, [id]
         )
 
         return ordenes.rows
