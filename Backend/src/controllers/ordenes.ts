070647bfdc08d0fded28ea0eb564eaba5769ec18
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-18 15:47:48 -0300

    desarrollando metodo getById en el controlador para ordenes

--- Backend/src/controllers/ordenes.ts
+++ Backend/src/controllers/ordenes.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { OrdenesModel } from "../models/ordenes.js";
+import { ValidateId } from "../schemas/productos.js";
 
 export class OrdenesController{
     static async getAll(req: Request, res: Response, next: NextFunction){
@@ -12,6 +13,19 @@ export class OrdenesController{
             }   
             const ordenesPorUsuario = await OrdenesModel.getByUsuario(usuario.id)
             return res.status(200).json(ordenesPorUsuario)
+        }catch(err){
+            return next(err)
+        }
+    }
+
+    static async getById(req: Request, res: Response, next: NextFunction){
+        const id = req.params.id as string
+        const validId = ValidateId(id)
+        if(!validId.success) return res.status(400).json({message: 'Datos invalidos'})
+        try{
+            const orden = await OrdenesModel.getById(validId.data)
+            if(orden === null) return res.status(404).json({message: 'No se encontro la orden'})
+            return res.status(200).json(orden)
         }catch(err){
             return next(err)
         }
