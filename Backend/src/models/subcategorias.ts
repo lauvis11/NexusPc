26f58b4254bef5e27161451ef19484b8afd1a213
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-23 13:16:23 -0300

    desarrollando metodo create del modelo para subcategorias

--- Backend/src/models/subcategorias.ts
+++ Backend/src/models/subcategorias.ts
@@ -1,4 +1,5 @@
 import { pool } from "../config/db.js";
+import type { SubcategoriaInput } from "../schemas/subcategorias.js";
 
 export class SubcategoriasModel{
     static async getAll({ categoria_id }: { categoria_id?: number } = {}){
@@ -14,5 +15,18 @@ export class SubcategoriasModel{
             `SELECT id, nombre, categoria_id FROM subcategoria ORDER BY nombre ASC`
         )
         return subcategorias.rows
+    }
+
+    static async create({ nombre, categoria_id }: SubcategoriaInput){
+        const categoriaExiste = await pool.query(
+            `SELECT id FROM categoria WHERE id = $1`, [categoria_id]
+        )
+        if(categoriaExiste.rows.length === 0) return null
+
+        const nuevaSubcategoria = await pool.query(
+            `INSERT INTO subcategoria(nombre, categoria_id) VALUES($1, $2)
+            RETURNING id, nombre, categoria_id`, [nombre, categoria_id]
+        )
+        return nuevaSubcategoria.rows[0]
     }
 }
