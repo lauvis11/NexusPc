e7687a41ebfa7efa6461d28d1d9546d7b39595dc
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-25 12:30:08 -0300

    agregando typo de estados de mercado pago

--- Backend/src/services/mercadopago.ts
+++ Backend/src/services/mercadopago.ts
@@ -1,5 +1,13 @@
 // SDK de Mercado Pago
-import { MercadoPagoConfig } from 'mercadopago';
+import { MercadoPagoConfig} from 'mercadopago';
 import { env } from '../config/env.js';
+import type { EstadoOrden } from '../schemas/ordenes.js';
 // Agrega credenciales
 export const client = new MercadoPagoConfig({ accessToken: env.TEST_ACCESS_TOKEN});
+
+export const mapaEstados: Record<string, EstadoOrden> = {
+    approved: 'PAGADO',
+    rejected: 'CANCELADA',
+    pending: 'PENDIENTE',
+    in_process: 'PENDIENTE'
+}
