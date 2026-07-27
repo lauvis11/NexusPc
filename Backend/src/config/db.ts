ce487ebe713e378236da60c1ffabd6a4ed9c3baf
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-27 16:46:50 -0300

    mejorando la configuracion de la db

--- Backend/src/config/db.ts
+++ Backend/src/config/db.ts
@@ -1,16 +1,13 @@
 import pg from "pg"
 import dotenv from "dotenv"
+import { env } from "./env.js"
 
 dotenv.config()
 
 const { Pool } = pg
 
-if (!process.env.DATABASE_URL) {
-    throw new Error('La url de la base de datos no está definida en las variables de entorno')
-}
-
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    connectionString: env.DATABASE_URL,
+    ssl: { rejectUnauthorized: true }
 })
 
