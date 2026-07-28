47b4b2845bd9e9c3e59a21f78ace702c41879ffc
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-28 14:40:42 -0300

    mojorando la robustez de contraseñas

--- Backend/src/schemas/auth.ts
+++ Backend/src/schemas/auth.ts
@@ -3,7 +3,7 @@ import z from "zod"
 const registerSchema = z.object({
     nombre: z.string().min(3),
     email: z.email(),
-    password: z.string().min(6).max(50) 
+    password: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres').max(50).regex(/[0-9]/, 'La contraseña debe contener al menos un número') 
 }) 
 
 const loginSchema = z.object({
--- Backend/src/schemas/usuarios.ts
+++ Backend/src/schemas/usuarios.ts
@@ -2,7 +2,7 @@ import z from "zod"
 
 const newPasswordSchema = z.object({
     password: z.string().min(6).max(50),
-    newPassword: z.string().min(6).max(50)
+    newPassword: z.string().min(8, 'La nueva contraseña debe tener al menos 8 caracteres').max(50).regex(/[0-9]/, 'La nueva contraseña debe contener al menos un número')
 })
 
 export function ValidateNewPassword(input: unknown){
