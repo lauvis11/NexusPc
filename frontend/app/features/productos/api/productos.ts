d27e6c7c8b038123dc8fcb678ffd76b5e09f032f
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-07-31 17:11:49 -0300

    desarrollando metodo get para ver un producto de la api

--- frontend/app/features/productos/api/productos.ts
+++ frontend/app/features/productos/api/productos.ts
@@ -1,13 +1,23 @@
-import type { ProductosResponse } from "../types/types"
+import type { Producto, ProductosResponse } from "../types/types"
 
-export async function getProductos(url: string, revalidate: number): Promise<ProductosResponse>{
-    try{
+export async function getProductos(url: string, revalidate: number): Promise<ProductosResponse> {
+    try {
         const res = await fetch(url, { next: { revalidate: revalidate } })
-        if(!res.ok) throw new Error('Error al obtener los productos')
+        if (!res.ok) throw new Error('Error al obtener los productos')
         const productos: ProductosResponse = await res.json()
         return productos
-    }catch{
-        return {data: [], pagination: {total: 0, page: 1, limit: 10, totalPages: 0}}
+    } catch {
+        return { data: [], pagination: { total: 0, page: 1, limit: 10, totalPages: 0 } }
     }
-} 
+}
+
+export async function getProductoIndividual(id: string): Promise<Producto> {
+    const url = `${process.env.NEXT_API_URL}/api/v1/productos/${id}`
+    const res = await fetch(url, {
+        next: {tags: [`producto-${id}`]}
+    })
+    if(!res.ok) throw new Error('Error al obtener el producto')
+    const producto: Producto = await res.json()
+    return producto
+}
 
