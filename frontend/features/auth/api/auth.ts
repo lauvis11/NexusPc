2b99e86f9e1f625ab796b78247fb4a4030446934
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-08-04 17:16:58 -0300

    desarrollando funcion login que llama a la api

--- frontend/features/auth/api/auth.ts
+++ frontend/features/auth/api/auth.ts
@@ -1,9 +1,26 @@
-// const API_URL = process.env.NEXT_API_URL
+import { LoginInput } from "@/features/usuarios/schemas/auth.schema"
 
-// export async function login(data){
-//     try{
-//         const response = await fetch()
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
+const API_URL = process.env.NEXT_API_URL
+
+export async function login(data: LoginInput){
+    try{
+        const response = await fetch(`${API_URL}/auth/login`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        if(!response.ok) {
+            const error = await response.json()
+            throw new Error(error.message)
+        }
+
+        const result = await response.json()
+        return result
+    }catch(error){
+        console.log(error)
+        return null
+    }
+}
