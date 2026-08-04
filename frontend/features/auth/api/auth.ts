fca65186076cafc9cc38fa3ef22e2ddc04eb5c0d
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-08-04 17:21:18 -0300

    desarrollando funcion logout que llama a la api

--- frontend/features/auth/api/auth.ts
+++ frontend/features/auth/api/auth.ts
@@ -3,24 +3,33 @@ import { LoginInput } from "@/features/usuarios/schemas/auth.schema"
 const API_URL = process.env.NEXT_API_URL
 
 export async function login(data: LoginInput){
-    try{
-        const response = await fetch(`${API_URL}/auth/login`, {
-            method: 'POST',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        if(!response.ok) {
-            const error = await response.json()
-            throw new Error(error.message)
-        }
-
-        const result = await response.json()
-        return result
-    }catch(error){
-        console.log(error)
-        return null
+    const response = await fetch(`${API_URL}/auth/login`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    if (!response.ok) {
+        const error = await response.json()
+        throw new Error(error.message)
     }
+
+    const result = await response.json()
+    return result
+}
+
+export async function logout(){
+    const response = await fetch(`${API_URL}/auth/logout`, {
+        method: 'POST',
+        credentials: 'include',
+    })
+    if(!response.ok){
+        const error = await response.json()
+        throw new Error(error.message)
+    }
+     
+    const result = await response.json()
+    return result
 }
