3bc5f0486c8668b6ecd031c0da332db5d06680af
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-08-06 16:18:33 -0300

    definiendo tipos para usuario y para el AuthContext

---
+++ frontend/features/auth/types/auth.ts
@@ -0,0 +1,18 @@
+export interface Usuario{
+    id: number
+    nombre: string
+    email: string
+    created_at: string
+    nombre_completo: string | null
+    dni: string | null
+    direccion: string | null
+    ciudad: string | null
+    provincia: string | null
+    codigo_postal: string | null
+}
+
+export interface AuthContextType{
+    user: Usuario | null
+    isLoading: boolean
+    logout: () => Promise<void>
+}
