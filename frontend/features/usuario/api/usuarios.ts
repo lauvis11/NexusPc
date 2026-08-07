e25dcd8a46c906b8b6ed619e253e54128dde818d
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-08-07 16:22:02 -0300

    desarrollando funcion para crear y actualizar los datos de facturacion llamando a la api

--- frontend/features/usuario/api/usuarios.ts
+++ frontend/features/usuario/api/usuarios.ts
@@ -1,4 +1,5 @@
 import { Usuario } from "@/features/auth/types/auth";
+import { DatosFacturacion } from "../types/usuarios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -8,4 +9,36 @@ export async function getPerfil(): Promise<Usuario | null> {
     })
     if (!response.ok) return null
     return response.json();
+}
+
+export async function crearDatosFacturacion(formData: DatosFacturacion){
+    const response = await fetch(`${API_URL}/usuarios/datos-facturacion`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+    if(!response.ok){
+        const error = await response.json();
+        throw new Error(error.message)
+    }
+    return response.json()
+}
+
+export async function actualizarDatos(formData: Partial<DatosFacturacion>){
+    const response = await fetch(`${API_URL}/usuarios/datos-facturacion`, {
+        method: "PATCH",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+    if(!response.ok){
+        const error = await response.json();
+        throw new Error(error.message)
+    }
+    return response.json()
 }
---
+++ frontend/features/usuario/types/usuarios.ts
@@ -0,0 +1,8 @@
+export interface DatosFacturacion {
+  nombre_completo: string
+  dni: string
+  direccion: string
+  ciudad: string
+  provincia: string
+  codigo_postal: string
+}
