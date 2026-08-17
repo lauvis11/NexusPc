6c7a697d4b7e2b5904e8f399460dec062e32f1ce
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-08-17 12:52:59 -0300

    desarrollando funcion para crear la preferencia de pago

--- frontend/features/ordenes/api/ordenes.ts
+++ frontend/features/ordenes/api/ordenes.ts
@@ -1,5 +1,5 @@
 import { API_URL } from "@/lib/constants";
-import type { Orden, OrdenInput } from "../types/ordenes";
+import type { InitPoint, Orden, OrdenInput } from "../types/ordenes";
 
 export async function crearOrden(data: OrdenInput): Promise<Orden> {
   const response = await fetch(`${API_URL}/ordenes`, {
@@ -14,6 +14,20 @@ export async function crearOrden(data: OrdenInput): Promise<Orden> {
   if(!response.ok) {
     const error = await response.json();
     throw new Error(error.message || "Error al crear la orden")
+  }
+
+  return response.json();
+}
+
+export async function crearPreferenciaPago(ordenId: string): Promise<InitPoint>{
+  const response = await fetch(`${API_URL}/ordenes/${ordenId}/pago`, {
+    method: "POST",
+    credentials: "include",
+  })
+
+  if(!response.ok){
+    const error = await response.json();
+    throw new Error(error.message || "Error al crear la preferencia de pago")
   }
 
   return response.json();
