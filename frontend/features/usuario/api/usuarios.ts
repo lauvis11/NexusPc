937c17d53cd6f9d7cf93a16e14344ad7e9cd97e4
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-08-21 16:35:33 -0300

    desarollando llamada a la api para cambiar la contraseña

--- frontend/features/usuario/api/usuarios.ts
+++ frontend/features/usuario/api/usuarios.ts
@@ -1,5 +1,5 @@
 import { Usuario } from "@/features/auth/types/auth";
-import { DatosFacturacion } from "../types/usuarios";
+import { ContraseñasForm, DatosFacturacion } from "../types/usuarios";
 import { API_URL } from "@/lib/constants";
 
 export async function getPerfil(): Promise<Usuario | null> {
@@ -38,6 +38,22 @@ export async function actualizarDatos(formData: Partial<DatosFacturacion>){
             "Content-Type": "application/json"
         },
         body: JSON.stringify(formData)
+    })
+    if(!response.ok){
+        const error = await response.json();
+        throw new Error(error.message)
+    }
+    return response.json()
+}
+
+export async function cambiarContraseña(data: ContraseñasForm){
+    const response = await fetch(`${API_URL}/auth/update-password`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
     })
     if(!response.ok){
         const error = await response.json();
