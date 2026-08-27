b6c432741ec59ae2a68b0c0ae028b9ee79c0d4e0
Lautaro Viscarra <lauvisneymi11@gmail.com>
2026-08-27 11:33:14 -0300

    definiendo tipos para productos

---
+++ frontend/features/admin/types/productos.ts
@@ -0,0 +1,17 @@
+export interface CaracteristicaInput {
+  clave: string;
+  valor: string;
+}
+
+export interface ProductoInput {
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  stock: number;
+  img_url: string;
+  public_id: string;
+  categoria_id: number;
+  subcategoria_id?: number | null;
+  destacado?: boolean;
+  caracteristicas: CaracteristicaInput[];
+}
